968c4e6890e31bd648b035c2dcba2c913f524030
sonuprasad66 <sp.srp1999@gmail.com>
2022-12-21 10:06:28 +0530

    backend

---
+++ Take_Live_Calls/backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+require("dotenv").config();
+const { connection } = require("./Config/db");
+const { userRouter } = require("./Routes/User.Route");
+
+app.get("/", (req, res) => {
+  res.send("Welcome to MainRoutes");
+});
+
+app.use("/", userRouter);
+
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log("Database connected Successful");
+    console.log(`App listening on ${process.env.PORT}`);
+  } catch (err) {
+    console.log("Database connected Failed");
+    console.log(err);
+  }
+});
